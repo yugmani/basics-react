698e21cfc1a8b969e77cfdbe8a96b72eee7d1612
yugmani <yugmani@yahoo.com>
2022-08-03 15:22:29 -0700

    destructuring props in functional components

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,13 +6,39 @@ import React from 'react';
 // };
 
 //default export
+// const Greet = (props) => {
+//   return (
+//     <div>
+//       <h1>
+//         Hello {props.name} from {props.location}
+//       </h1>
+//       {props.children}
+//     </div>
+//   );
+// };
+
+//Destructuring props
+// const Greet = ({ name, location, children }) => {
+//   return (
+//     <div>
+//       <h1>
+//         Hello {name} from {location}
+//       </h1>
+//       {children}
+//     </div>
+//   );
+// };
+
+//Alternative destructuring props
 const Greet = (props) => {
+  const { name, location, children } = props;
+  
   return (
     <div>
       <h1>
-        Hello {props.name} from {props.location}
+        Hello {name} from {location}
       </h1>
-      {props.children}
+      {children}
     </div>
   );
 };
