cde99f04c916c9cac5c0f4f51e0f89c185e990b3
yugmani <yugmani@yahoo.com>
2022-08-04 00:50:37 -0700

    Parent and Child Components

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBind from './components/EventBind';
+import Parent from './components/Parent';
 
 export default function App() {
   return (
     <div className="App">
-      <Greet name="Yoog" location="Los Angeles">
+      {/* <Greet name="Yoog" location="Los Angeles">
         <p>I love traveling</p>
-      </Greet>
+      </Greet> */}
       <Greet name="Noor Fateh" location="Canada" />
       <Welcome person="friends" group="MERN" />
       <Hello greet="Namaste" />
@@ -25,6 +26,7 @@ export default function App() {
       <FunctionClick />
       <ClassClick />
       <EventBind />
+      <Parent />
     </div>
   );
 }
---
+++ src/components/Child.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Child() {
+  return (
+    <div>
+      <button>Greet Parent</button>
+    </div>
+  );
+}
+
+export default Child;
